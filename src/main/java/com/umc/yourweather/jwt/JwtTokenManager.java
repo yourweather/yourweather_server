6b8921b38855276516b13a18ebe0f0367691520c
shon5544 <sbs8239@gmail.com>
2023-07-05 19:28:02 +0900

    refactor: 필요없는 필드(AUTH) 삭제

--- src/main/java/com/umc/yourweather/jwt/JwtTokenManager.java
+++ src/main/java/com/umc/yourweather/jwt/JwtTokenManager.java
@@ -28,7 +28,6 @@ public class JwtTokenManager {
     //jwt 토큰의 사용자 식별 claim으로는 email을 사용함
     private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
     private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
-    private static final String AUTH = "auth";
     private static final String EMAIL = "email";
     private static final String BEARER = "Bearer ";
 
