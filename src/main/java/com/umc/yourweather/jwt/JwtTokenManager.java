9e6083281bb4a451f548f5fcdae3337c8416b20a
shon5544 <sbs8239@gmail.com>
2023-07-05 19:21:53 +0900

    feat: 필드 추가

--- src/main/java/com/umc/yourweather/jwt/JwtTokenManager.java
+++ src/main/java/com/umc/yourweather/jwt/JwtTokenManager.java
@@ -1,5 +1,36 @@
 package com.umc.yourweather.jwt;
 
+import com.umc.yourweather.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class JwtTokenManager {
 
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${jwt.access.expiration}")
+    private Long accessTokenExpiration;
+
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshExpiration;
+
+    @Value("${jwt.access.header}")
+    private String accessTokenHeader;
+
+    @Value("${jwt.refresh.header}")
+    private String refreshTokenHeader;
+
+
+    //jwt 토큰의 사용자 식별 claim으로는 email을 사용함
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String AUTH = "auth";
+    private static final String EMAIL = "email";
+    private static final String BEARER = "Bearer ";
+
+    private final UserRepository userRepository;
 }
