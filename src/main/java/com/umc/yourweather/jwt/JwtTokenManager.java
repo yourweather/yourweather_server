f67535ed9776e7fcc6558f795ed779ae11e4aaf3
shon5544 <sbs8239@gmail.com>
2023-07-05 19:42:07 +0900

    feat: Slf4j 추가

--- src/main/java/com/umc/yourweather/jwt/JwtTokenManager.java
+++ src/main/java/com/umc/yourweather/jwt/JwtTokenManager.java
@@ -6,6 +6,7 @@ import com.umc.yourweather.domain.User;
 import com.umc.yourweather.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenManager {
 
     @Value("${jwt.secretKey}")
