ff1ed6cff7829ef1b84e2047a5b76e00e0d213f4
shon5544 <sbs8239@gmail.com>
2023-07-05 19:15:32 +0900

    feat: findByEmail 메서드 추가

--- src/main/java/com/umc/yourweather/repository/jpa/UserJpaRepository.java
+++ src/main/java/com/umc/yourweather/repository/jpa/UserJpaRepository.java
@@ -4,6 +4,9 @@ import com.umc.yourweather.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
