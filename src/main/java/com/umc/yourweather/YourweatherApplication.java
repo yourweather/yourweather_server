36979ec76fed5010b8cf21a8355f8c74d6e1f8aa
shon5544 <sbs8239@gmail.com>
2023-07-05 17:25:37 +0900

    Project setting

---
+++ src/main/java/com/umc/yourweather/YourweatherApplication.java
@@ -0,0 +1,13 @@
+package com.umc.yourweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/yourweather/YourweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.yourweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
