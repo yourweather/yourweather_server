4916e6b54519bbd5f56f4bcc40044e24d92d2f6c
shon5544 <sbs8239@gmail.com>
2023-07-07 04:25:49 +0900

    feat: attemptAuthentication 함수 내용 채움 ## 하는 일 - 요청 body를 긁어와서 email과 password를 받음. - 해당 정보들로 간단히 UsernamePasswordAuthenticationToken을 만듦. - UsernamePasswordAuthenticationToken은 Authentication을 상속받는 클래스다. - 얘는 쉽게 말해서 아직 인증은 안 된 Authentication 객체를 만드는 용도이다. - this.getAuthenticationManager().authenticate(authReq) 함수를 사용하는 시점에서 UserDetailsService가 작동한다. - 좀 더 복잡한 프로세스로 이뤄지지만 중략하고 결론부터 말하면 그렇다. - 아무튼, 그렇게 UserDetailsService를 작동시키면서 loadByUsername 메서드를 실행시킨다. - UserDetailsService의 loadUserByUsername 메서드로 우리의 Username..중략..Token의 이름부분(코드에선 email이 이름 역할을 함)이 제대로 DB에 있는 유저인지 검증하는 것이다. - 그런 식으로 인증이 잘 완료되면, 세션에 인증정보가 올라간다!

--- src/main/java/com/umc/yourweather/jwt/filter/CustomLoginFilter.java
+++ src/main/java/com/umc/yourweather/jwt/filter/CustomLoginFilter.java
@@ -4,19 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class CustomLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     private static final String DEFAULT_LOGIN_REQUEST_URI = "/login";
     private static final String HTTP_METHOD = "POST";
     private static final String CONTENT_TYPE = "application/json";
-    private static final String USERNAME_KEY = "email";
+    private static final String EMAIL_KEY = "email";
     private static final String PASSWORD_KEY = "password";
 
     private ObjectMapper objectMapper;
@@ -31,6 +36,21 @@ public class CustomLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
-        return null;
+        // 여기서 request의 body를 ObjectMapper로 읽고 로그인 처리를 해주는 것!
+
+        // contentType이 기재되지 않았거나 application/json이 아니면 에러를 던진다.
+        if(request.getContentType() == null || !request.getContentType().equals(CONTENT_TYPE)) {
+            throw new AuthenticationServiceException("지원되지 않는 Content-Type입니다. " + request.getContentType());
+        }
+
+        String body = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+        Map<String, String> mappedBody = objectMapper.readValue(body, Map.class);
+
+        String email = mappedBody.get(EMAIL_KEY);
+        String password = mappedBody.get(PASSWORD_KEY);
+
+        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(email, password);
+        return this.getAuthenticationManager().authenticate(authReq);
     }
 }
