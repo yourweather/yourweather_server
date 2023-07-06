013b70e96448357ad0cc1e83b596f6f2640f5143
shon5544 <sbs8239@gmail.com>
2023-07-06 22:41:36 +0900

    feat: 필드 변수, 메서드 추가

--- src/main/java/com/umc/yourweather/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/umc/yourweather/jwt/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,29 @@
 package com.umc.yourweather.jwt.filter;
 
-public class JwtAuthenticationFilter{
+import com.umc.yourweather.jwt.JwtTokenManager;
+import com.umc.yourweather.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    // login 요청이 들어오면 필터에서 토큰 검증 건너뜀.
+    private static final String NO_CHECK_URI = "/login";
+
+    private final JwtTokenManager jwtTokenManager;
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    }
 }
