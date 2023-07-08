07794b35c4f8aa20a070013b2ccf5f0ba297166a
shon5544 <sbs8239@gmail.com>
2023-07-08 23:08:03 +0900

    feat: 메서드 구현 - 참고로 DI 받아야지만 이 친구가 작동하기 때문에 스프링 빈으로 등록했습니다.

--- src/main/java/com/umc/yourweather/auth/CustomUserDetailsService.java
+++ src/main/java/com/umc/yourweather/auth/CustomUserDetailsService.java
@@ -1,4 +1,19 @@
 package com.umc.yourweather.auth;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    // loadUserByUsername이지만, CustomLoginFilter에서 email을 username으로 삽입했기 때문에
+    // 실상은 loadUserByEmail이라고 생각해도 무방합니다.
+    // 함수명은 메서드 구현때문에 어쩔 수 없음.
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return null;
+    }
 }
