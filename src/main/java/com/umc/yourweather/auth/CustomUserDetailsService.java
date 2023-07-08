f95e42ceb9105325873f3c72d08a15970978a843
shon5544 <sbs8239@gmail.com>
2023-07-08 23:09:10 +0900

    feat: userRepository 필드 추가

--- src/main/java/com/umc/yourweather/auth/CustomUserDetailsService.java
+++ src/main/java/com/umc/yourweather/auth/CustomUserDetailsService.java
@@ -1,13 +1,16 @@
 package com.umc.yourweather.auth;
 
+import com.umc.yourweather.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-
+    private final UserRepository userRepository;
 
     // loadUserByUsername이지만, CustomLoginFilter에서 email을 username으로 삽입했기 때문에
     // 실상은 loadUserByEmail이라고 생각해도 무방합니다.
