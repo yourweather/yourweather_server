63ae59047cc559c5a71505d0af0ba57fc1e97f05
shon5544 <sbs8239@gmail.com>
2023-07-08 23:15:30 +0900

    feat: 필드 추가

--- src/main/java/com/umc/yourweather/jwt/handler/LoginSuccessHandler.java
+++ src/main/java/com/umc/yourweather/jwt/handler/LoginSuccessHandler.java
@@ -1,5 +1,17 @@
 package com.umc.yourweather.jwt.handler;
 
+import com.umc.yourweather.jwt.JwtTokenManager;
+import com.umc.yourweather.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
+@Slf4j
+@RequiredArgsConstructor
 public class LoginSuccessHandler {
+    private final JwtTokenManager jwtTokenManager;
+    private final UserRepository userRepository;
+
+    @Value("${jwt.access.expiration}")
+    private String accessTokenExpiration;
 }
