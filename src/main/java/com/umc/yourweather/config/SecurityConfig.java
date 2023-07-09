5c9593d5744b8f30cf6e063a549d85e24e949f2b
shon5544 <sbs8239@gmail.com>
2023-07-09 16:54:16 +0900

    feat: PasswordEncoder 빈 등록

--- src/main/java/com/umc/yourweather/config/SecurityConfig.java
+++ src/main/java/com/umc/yourweather/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
@@ -54,5 +56,10 @@ public class SecurityConfig {
         http.addFilterBefore(jwtAuthenticationFilter, CustomLoginFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
