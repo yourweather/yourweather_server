ea9d7f634febdb8f8977235cac54ee38c736f4b5
shon5544 <sbs8239@gmail.com>
2023-07-09 17:11:14 +0900

    fix: 각 핸들러 빈으로 만듦

--- src/main/java/com/umc/yourweather/jwt/handler/LoginSuccessHandler.java
+++ src/main/java/com/umc/yourweather/jwt/handler/LoginSuccessHandler.java
@@ -13,11 +13,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtTokenManager jwtTokenManager;
     private final UserRepository userRepository;
