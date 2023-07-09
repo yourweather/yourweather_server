71915cda9b1b5eba0a79910472138aeaf09189d9
shon5544 <sbs8239@gmail.com>
2023-07-09 16:27:07 +0900

    feat: SecurityFilterChain Bean 추가

--- src/main/java/com/umc/yourweather/config/SecurityConfig.java
+++ src/main/java/com/umc/yourweather/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package com.umc.yourweather.config;
 import com.umc.yourweather.auth.CustomUserDetailsService;
 import com.umc.yourweather.jwt.JwtTokenManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +16,9 @@ public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
     private final JwtTokenManager jwtTokenManager;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
 }
