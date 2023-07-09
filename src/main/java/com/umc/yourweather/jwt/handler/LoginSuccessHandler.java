e75fa922752e5f4a7bbedda1dde499bf02bc0c05
shon5544 <sbs8239@gmail.com>
2023-07-09 19:12:13 +0900

    fix: Transactional 애노테이션 추가 - 유저의 refresh Token을 더티체킹해주기 위해서 이런 코드를 삽입

--- src/main/java/com/umc/yourweather/jwt/handler/LoginSuccessHandler.java
+++ src/main/java/com/umc/yourweather/jwt/handler/LoginSuccessHandler.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
@@ -27,6 +28,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Value("${jwt.access.expiration}")
     private String accessTokenExpiration;
 
+    @Transactional
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
