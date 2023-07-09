7dc37acbd5b3b158ae3acbe78f539d34d072600b
shon5544 <sbs8239@gmail.com>
2023-07-09 16:29:36 +0900

    feat: formLogin disable

--- src/main/java/com/umc/yourweather/config/SecurityConfig.java
+++ src/main/java/com/umc/yourweather/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,6 +20,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
