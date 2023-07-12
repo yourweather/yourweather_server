1aa68178021c932df983909b1d384a1af080c565
shon5544 <sbs8239@gmail.com>
2023-07-12 19:14:03 +0900

    feat: EmailService sendMessage 메서드 추가

--- src/main/java/com/umc/yourweather/service/EmailService.java
+++ src/main/java/com/umc/yourweather/service/EmailService.java
@@ -1,5 +1,26 @@
 package com.umc.yourweather.service;
 
-public class EmailService {
+import com.umc.yourweather.domain.MessageCreator;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender emailSender;
+
+    public String sendMessage(String to)throws Exception {
+        MessageCreator messageCreator = new MessageCreator(emailSender);
+        MimeMessage message = messageCreator.createMessage(to);
+        try{//예외처리
+            emailSender.send(message);
+        }catch(MailException es){
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return messageCreator.getCode();
+    }
 }
