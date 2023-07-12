9e583bf5f181a8c1ab7bcb8b2c4eb6ce06205b0b
shon5544 <sbs8239@gmail.com>
2023-07-12 19:12:46 +0900

    feat: createKey 메서드 추가

--- src/main/java/com/umc/yourweather/domain/MessageCreator.java
+++ src/main/java/com/umc/yourweather/domain/MessageCreator.java
@@ -14,11 +14,29 @@ import org.springframework.mail.javamail.JavaMailSender;
 public class MessageCreator {
     private final JavaMailSender emailSender;
 
-    private final String code;
+    private final String code = createKey();
 
     @Value("${AdminMail.id}")
     private String id;
 
+    public static String createKey() {
+        StringBuilder key = new StringBuilder();
+        Random rnd = new Random();
 
+        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤
 
+            switch (index) {
+                //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+                case 0 -> key.append((char) ((int) (rnd.nextInt(26)) + 97));
+
+                //  A~Z
+                case 1 -> key.append((char) ((int) (rnd.nextInt(26)) + 65));
+
+                // 0~9
+                case 2 -> key.append((rnd.nextInt(10)));
+            }
+        }
+        return key.toString();
+    }
 }
