a372b057a529f27728b1a3048f78aff84da38540
shon5544 <sbs8239@gmail.com>
2023-07-12 19:09:14 +0900

    feat: MessageCreator 필드 추가

--- src/main/java/com/umc/yourweather/domain/MessageCreator.java
+++ src/main/java/com/umc/yourweather/domain/MessageCreator.java
@@ -1,5 +1,24 @@
 package com.umc.yourweather.domain;
 
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@RequiredArgsConstructor
 public class MessageCreator {
+    private final JavaMailSender emailSender;
+
+    private final String code;
+
+    @Value("${AdminMail.id}")
+    private String id;
+
+
 
 }
