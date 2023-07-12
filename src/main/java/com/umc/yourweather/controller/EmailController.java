762aceba5c489cb63de429fd4edf78e5b012526c
shon5544 <sbs8239@gmail.com>
2023-07-12 19:40:39 +0900

    feat: logging 추가

--- src/main/java/com/umc/yourweather/controller/EmailController.java
+++ src/main/java/com/umc/yourweather/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.umc.yourweather.controller;
 import com.umc.yourweather.dto.ResponseDto;
 import com.umc.yourweather.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/email")
 @RequiredArgsConstructor
+@Slf4j
 public class EmailController {
     private final EmailService emailService;
 
@@ -19,6 +21,7 @@ public class EmailController {
     public ResponseDto<Void> send(@RequestParam String email) {
         try {
             emailService.sendMessage(email);
+            log.info("이메일 전송 완료");
             return ResponseDto.success("이메일 전송 완료.");
         } catch (Exception e) {
             return ResponseDto.fail(HttpStatus.BAD_REQUEST, e.getMessage());
--- src/main/java/com/umc/yourweather/service/EmailService.java
+++ src/main/java/com/umc/yourweather/service/EmailService.java
@@ -3,18 +3,21 @@ package com.umc.yourweather.service;
 import com.umc.yourweather.domain.MessageCreator;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
     private final JavaMailSender emailSender;
 
     public String sendMessage(String to) throws Exception {
         MessageCreator messageCreator = new MessageCreator(emailSender);
         MimeMessage message = messageCreator.createMessage(to);
+        log.info("message 생성: " + message.getMessageID());
         try{//예외처리
             emailSender.send(message);
         }catch(MailException es){
