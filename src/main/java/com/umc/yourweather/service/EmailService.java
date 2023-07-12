6d2098bb1dc0e95d6a3c865e71c3724c88932bcd
shon5544 <sbs8239@gmail.com>
2023-07-12 19:35:51 +0900

    feat: exception 메세지 추가

--- src/main/java/com/umc/yourweather/service/EmailService.java
+++ src/main/java/com/umc/yourweather/service/EmailService.java
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender emailSender;
 
-    public String sendMessage(String to)throws Exception {
+    public String sendMessage(String to) throws Exception {
         MessageCreator messageCreator = new MessageCreator(emailSender);
         MimeMessage message = messageCreator.createMessage(to);
         try{//예외처리
             emailSender.send(message);
         }catch(MailException es){
             es.printStackTrace();
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("email 전송 실패: 받는 사람 이메일을 다시 확인해주세요");
         }
         return messageCreator.getCode();
     }
