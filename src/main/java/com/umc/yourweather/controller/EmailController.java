19278669b6d51b5f85bd36dc174c2d58dfc3f2a6
shon5544 <sbs8239@gmail.com>
2023-07-12 19:36:55 +0900

    feat: EmailController send 메서드 추가

--- src/main/java/com/umc/yourweather/controller/EmailController.java
+++ src/main/java/com/umc/yourweather/controller/EmailController.java
@@ -1,17 +1,27 @@
 package com.umc.yourweather.controller;
 
 import com.umc.yourweather.dto.ResponseDto;
+import com.umc.yourweather.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/email")
+@RequiredArgsConstructor
 public class EmailController {
+    private final EmailService emailService;
 
     @GetMapping("/send")
-    public ResponseDto<Void> send() {
-
-        return ResponseDto.success("이메일 전송 완료.");
+    public ResponseDto<Void> send(@RequestParam String email) {
+        try {
+            emailService.sendMessage(email);
+            return ResponseDto.success("이메일 전송 완료.");
+        } catch (Exception e) {
+            return ResponseDto.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 }
