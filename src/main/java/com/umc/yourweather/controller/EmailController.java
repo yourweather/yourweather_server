ca8b119c009a00e099491c93f020ca82ce0fc148
shon5544 <sbs8239@gmail.com>
2023-07-12 18:09:29 +0900

    add: send 메서드 추가

--- src/main/java/com/umc/yourweather/controller/EmailController.java
+++ src/main/java/com/umc/yourweather/controller/EmailController.java
@@ -1,5 +1,17 @@
 package com.umc.yourweather.controller;
 
+import com.umc.yourweather.dto.ResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/email")
 public class EmailController {
 
+    @GetMapping("/send")
+    public ResponseDto<Void> send() {
+
+        return ResponseDto.success("이메일 전송 완료.");
+    }
 }
