d6daa29a591d848da3201ed62fad489d413c7e30
shon5544 <sbs8239@gmail.com>
2023-07-13 17:46:34 +0900

    fix: api 경로 수정

--- src/main/java/com/umc/yourweather/controller/UserController.java
+++ src/main/java/com/umc/yourweather/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final UserService userService;
