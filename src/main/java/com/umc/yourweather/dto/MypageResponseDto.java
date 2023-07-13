76d3d4d259c97d4ab7c0aa0f268bfb1452457b9a
gundorit <packdev937@gmail.com>
2023-07-13 19:47:54 +0900

    feat : @AllArgsConstructor 추가

--- src/main/java/com/umc/yourweather/dto/MypageResponseDto.java
+++ src/main/java/com/umc/yourweather/dto/MypageResponseDto.java
@@ -1,8 +1,10 @@
 package com.umc.yourweather.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class MypageResponseDto {
 
     private String nickname;
