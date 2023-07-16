cf68ac2d1a710fbf29b1e0df75afcede9b7aaaa4
gundorit <packdev937@gmail.com>
2023-07-16 17:38:28 +0900

    feat : add GlobalExceptionHandler

---
+++ src/main/java/com/umc/yourweather/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.umc.yourweather.exception;
+
+import com.umc.yourweather.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseDto<?> handler(Exception e) {
+        return ResponseDto.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+}
