735a6db9cfebc35cece7a6a2fa86e9d464976a4d
shon5544 <sbs8239@gmail.com>
2023-07-16 22:24:50 +0900

    feat: StringRedisTemplate 의존성 주입

--- src/main/java/com/umc/yourweather/repository/redis/EmailCodeRedisRepository.java
+++ src/main/java/com/umc/yourweather/repository/redis/EmailCodeRedisRepository.java
@@ -1,5 +1,12 @@
 package com.umc.yourweather.repository.redis;
 
-public class EmailCodeRedisRepository {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
+public class EmailCodeRedisRepository {
+    private final StringRedisTemplate template;
 }
