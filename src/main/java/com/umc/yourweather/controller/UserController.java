debb5b1920462277aef0da269734bc8b48115d4a
gundorit <packdev937@gmail.com>
2023-07-16 19:19:29 +0900

    feat : 회원 탈퇴 API 매핑

--- src/main/java/com/umc/yourweather/controller/UserController.java
+++ src/main/java/com/umc/yourweather/controller/UserController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,12 @@ public class UserController {
         @RequestBody @Valid ChangePasswordDto changePasswordDto,
         @AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseDto.success(userService.changePassword(changePasswordDto, userDetails));
+    }
+
+    @PutMapping("/withdraw")
+    public ResponseDto<UserResponseDto> withdraw(
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseDto.success(userService.withdraw(userDetails));
     }
 }
 
