bec89b5f37fe1abc55ce7bd39fa4976db1a07a36
gundorit <packdev937@gmail.com>
2023-07-19 16:47:00 +0900

    feat : 메모와 일대다 관계 추가

--- src/main/java/com/umc/yourweather/domain/Weather.java
+++ src/main/java/com/umc/yourweather/domain/Weather.java
@@ -2,6 +2,8 @@ package com.umc.yourweather.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import java.util.LinkedList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,34 +22,22 @@ public class Weather {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "weather_id")
     private Long id;
-
     private String email; //PK
-
-    private Status status; //날씨 상태 enums
-
-    private Double temperature; //온도
-    private String diary; //diary? comment?기
-
     @NotBlank
     private String datetime; //yyyyMMDD 형식으로 받아와서, 파싱해서 넣을 예정
-
     private Integer year;
     private Integer month;
     private Integer day;
+
+    @OneToMany(mappedBy = "weather")
+    List<Memo> memos = new LinkedList<>();
 
     LocalDate date = LocalDate.parse(datetime, java.time.format.DateTimeFormatter.BASIC_ISO_DATE);
 
     @Builder
     public Weather(String email,
-        Status status,
-        Double temperature,
-        String diary,
         String datetime) {
-
         this.email = email;
-        this.status = status;
-        this.temperature = temperature;
-        this.diary = diary;
         this.datetime = datetime; //yyyyMMDD
         this.year = date.getYear();
         this.month = date.getMonthValue();
