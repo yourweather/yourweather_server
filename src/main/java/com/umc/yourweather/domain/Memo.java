29fb5af22351576a7b051a00b2620ced581c7c3e
gundorit <packdev937@gmail.com>
2023-07-19 16:47:19 +0900

    feat : 동적으로 추가될 메모 엔티티 생성

---
+++ src/main/java/com/umc/yourweather/domain/Memo.java
@@ -0,0 +1,27 @@
+package com.umc.yourweather.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memo_id")
+    private Long id;
+    private Status status; //날씨 상태 enums
+    private String time;
+    private int condition;
+    private String content;
+
+
+}
