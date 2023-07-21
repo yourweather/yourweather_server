1e92010434fbb3d0dffeb6d5482223e591caa667
gundorit <packdev937@gmail.com>
2023-07-22 00:38:37 +0900

    fix : change from LocalDateTime to LocalTime

--- src/main/java/com/umc/yourweather/domain/Memo.java
+++ src/main/java/com/umc/yourweather/domain/Memo.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,7 +28,7 @@ public class Memo {
 
     @Enumerated(EnumType.STRING)
     private Status status; //날씨 상태 enums
-    private LocalDateTime time;
+    private LocalTime time;
     private int condition;
     private String content;
 
