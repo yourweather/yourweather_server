43938b16e3eaac86b7c18172201dbf6e377aff52
gundorit <packdev937@gmail.com>
2023-07-22 00:41:43 +0900

    add : MemoControllerTest.java

---
+++ src/test/java/com/umc/yourweather/controller/MemoControllerTest.java
@@ -0,0 +1,11 @@
+package com.umc.yourweather.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoControllerTest {
+
+
+}
