8794fe40fb4fadb6e53d381e3c2a04edcfc94cd3
gundorit <packdev937@gmail.com>
2023-07-23 00:35:42 +0900

    feat : add fields in HomeResponseDto (NOT DONE)

--- src/main/java/com/umc/yourweather/dto/HomeResponseDto.java
+++ src/main/java/com/umc/yourweather/dto/HomeResponseDto.java
@@ -1,5 +1,24 @@
 package com.umc.yourweather.dto;
 
+import com.umc.yourweather.domain.Status;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class HomeResponseDto {
 
+    // 홈 화면은 미정, 후에 픽스되는 대로 필드를 추가할 예정
+    private String nickname;
+    private Status status;
+    private int condition;
+
+    @Builder
+    public HomeResponseDto(String nickname, Status status, int condition) {
+        this.nickname = nickname;
+        this.status = status;
+        this.condition = condition;
+    }
 }
+
+// List -> Json 관련 주요 사항
+// 필드들은 ObjectMapper가 사용 가능한 접근자 메서드(Getter)를 통해 접근하여 JSON으로 변환됩니다.
