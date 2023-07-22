8451eaec7490361e345c2b73dd8c6627dc13869e
gundorit <packdev937@gmail.com>
2023-07-22 14:20:15 +0900

    feat : add Validation

--- src/main/java/com/umc/yourweather/dto/MemoRequestDto.java
+++ src/main/java/com/umc/yourweather/dto/MemoRequestDto.java
@@ -1,15 +1,25 @@
 package com.umc.yourweather.dto;
 
 import com.umc.yourweather.domain.Status;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class MemoRequestDto {
 
+    @NotBlank
     Status status;
+
+    @NotBlank
     String content;
+
+    @NotBlank
     int condition;
+
+    @NotBlank
     int year;
+    @NotBlank
     int month;
+    @NotBlank
     int day;
 }
