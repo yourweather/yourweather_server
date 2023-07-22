24a2db6e4830d109c1f32570628636628a7df926
gundorit <packdev937@gmail.com>
2023-07-22 15:46:06 +0900

    feat : add custom findBy methods
    findAllByYearAndMonth, findByYearAndMonthAndDay

--- src/main/java/com/umc/yourweather/repository/WeatherRepository.java
+++ src/main/java/com/umc/yourweather/repository/WeatherRepository.java
@@ -1,8 +1,13 @@
 package com.umc.yourweather.repository;
 
 import com.umc.yourweather.domain.Weather;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
 
+    List<Weather> findAllByYearAndMonth(int year, int month);
+
+    Optional<Weather> findByYearAndMonthAndDay(int year, int month, int day);
 }
