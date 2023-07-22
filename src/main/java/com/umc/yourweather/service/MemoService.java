d20d6e3e65fa3bb9e3cd4a8040a4e38a531513dd
gundorit <packdev937@gmail.com>
2023-07-23 00:33:31 +0900

    feat : add write() to save memo

--- src/main/java/com/umc/yourweather/service/MemoService.java
+++ src/main/java/com/umc/yourweather/service/MemoService.java
@@ -1,5 +1,36 @@
 package com.umc.yourweather.service;
 
+import com.umc.yourweather.auth.CustomUserDetails;
+import com.umc.yourweather.domain.Memo;
+import com.umc.yourweather.domain.Weather;
+import com.umc.yourweather.dto.MemoRequestDto;
+import com.umc.yourweather.repository.MemoRepository;
+import com.umc.yourweather.repository.WeatherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemoService {
 
+    private final WeatherRepository weatherRepository;
+    private final MemoRepository memoRepository;
+
+    public Memo write(MemoRequestDto memoRequestDto, CustomUserDetails userDetails) {
+        Weather weather = weatherRepository.findByYearAndMonthAndDay(memoRequestDto.getDate().getYear(),
+                memoRequestDto.getDate().getMonthValue(), memoRequestDto.getDate().getDayOfMonth())
+            .orElseThrow(() -> new RuntimeException("해당 날자에 날씨 객체가 존재하지 않습니다."));
+
+        // MemoRequestDto에 넘어온 정보를 토대로 Memo 객체 생성
+        Memo memo = Memo.builder()
+            .weather(weather)
+            .status(memoRequestDto.getStatus())
+            .content(memoRequestDto.getContent())
+            .condition(memoRequestDto.getCondition())
+            .build();
+
+        weather.addMemos(memo);
+        memoRepository.save(memo);
+        return memo;
+    }
 }
