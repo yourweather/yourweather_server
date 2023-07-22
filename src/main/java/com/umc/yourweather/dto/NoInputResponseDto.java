c578a5907ab5ac222db4f44f147db5a1af437879
gundorit <packdev937@gmail.com>
2023-07-22 15:48:39 +0900

    feat : add List<LocalDate> localDates to transfer no-inpus dates

--- src/main/java/com/umc/yourweather/dto/NoInputResponseDto.java
+++ src/main/java/com/umc/yourweather/dto/NoInputResponseDto.java
@@ -1,5 +1,14 @@
 package com.umc.yourweather.dto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NoInputResponseDto {
 
+    List<LocalDate> localDates = new ArrayList<>();
+
+    public void addDate(LocalDate localDate) {
+        localDates.add(localDate);
+    }
 }
