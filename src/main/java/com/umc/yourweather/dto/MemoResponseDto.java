dc0d4af4c1f6e7b9a9d296c58f831612a7fe27f7
gundorit <packdev937@gmail.com>
2023-07-23 00:39:50 +0900

    add : MemoResponseDto.java

---
+++ src/main/java/com/umc/yourweather/dto/MemoResponseDto.java
@@ -0,0 +1,20 @@
+package com.umc.yourweather.dto;
+
+import com.umc.yourweather.domain.Status;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+
+    private Status status;
+    private String content;
+    private int condition;
+
+    @Builder
+    public MemoResponseDto(Status status, String content, int condition) {
+        this.status = status;
+        this.content = content;
+        this.condition = condition;
+    }
+}
