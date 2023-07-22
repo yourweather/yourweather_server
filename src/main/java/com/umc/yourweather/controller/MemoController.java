b9201e824dd7be0dbd5bd1f1174976b125161427
gundorit <packdev937@gmail.com>
2023-07-23 00:40:13 +0900

    feat : add write()

--- src/main/java/com/umc/yourweather/controller/MemoController.java
+++ src/main/java/com/umc/yourweather/controller/MemoController.java
@@ -1,5 +1,30 @@
 package com.umc.yourweather.controller;
 
+import com.umc.yourweather.api.RequestURI;
+import com.umc.yourweather.auth.CustomUserDetails;
+import com.umc.yourweather.domain.Memo;
+import com.umc.yourweather.dto.MemoRequestDto;
+import com.umc.yourweather.dto.MemoResponseDto;
+import com.umc.yourweather.dto.ResponseDto;
+import com.umc.yourweather.service.MemoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(RequestURI.MEMO_URI)
 public class MemoController {
 
+    private final MemoService memoService;
+
+    @PostMapping("/write")
+    public ResponseDto<MemoResponseDto> write(@RequestBody @Valid MemoRequestDto memoRequestDto
+        , @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseDto.success("메모 저장 완료", memoService.write(memoRequestDto, userDetails));
+    }
 }
