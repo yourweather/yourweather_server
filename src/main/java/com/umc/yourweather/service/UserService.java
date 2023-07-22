8b38a3bd9c822e8f84bcf49714ac8510ba53b97b
gundorit <packdev937@gmail.com>
2023-07-22 13:38:55 +0900

    fix : add meaningful Exception Handling

--- src/main/java/com/umc/yourweather/service/UserService.java
+++ src/main/java/com/umc/yourweather/service/UserService.java
@@ -6,6 +6,7 @@ import com.umc.yourweather.domain.User;
 import com.umc.yourweather.dto.ChangePasswordDto;
 import com.umc.yourweather.dto.UserResponseDto;
 import com.umc.yourweather.dto.SignupRequestDto;
+import com.umc.yourweather.exception.UserNotFoundException;
 import com.umc.yourweather.repository.UserRepository;
 import jakarta.validation.Valid;
 import java.util.UUID;
@@ -37,7 +38,7 @@ public class UserService {
 
         // 이메일 중복 검증 로직
         if (userRepository.findByEmail(email).isPresent()) {
-            throw new IllegalArgumentException("이미 해당 이메일로 가입된 유저가 존재합니다.");
+            throw new RuntimeException("이미 해당 이메일로 가입된 유저가 존재합니다.");
         }
 
         User user = User.builder()
@@ -54,14 +55,14 @@ public class UserService {
 
     public UserResponseDto mypage(CustomUserDetails userDetails) {
         User user = userRepository.findByEmail(userDetails.getUser().getEmail())
-            .orElseThrow(() -> new IllegalArgumentException("등록된 사용자가 없습니다."));
+            .orElseThrow(() -> new UserNotFoundException("등록된 사용자가 없습니다."));
         return new UserResponseDto(user.getNickname(), user.getEmail());
     }
 
     public String changePassword(ChangePasswordDto changePasswordDto,
         CustomUserDetails userDetails) {
         User user = userRepository.findByEmail(userDetails.getUser().getEmail())
-            .orElseThrow(() -> new IllegalArgumentException("등록된 사용자가 없습니다."));
+            .orElseThrow(() -> new UserNotFoundException("등록된 사용자가 없습니다."));
 
         user.changePassword(changePasswordDto.getPassword());
         return "비밀번호 변경 완료";
@@ -69,7 +70,7 @@ public class UserService {
 
     public String withdraw(CustomUserDetails userDetails) {
         User user = userRepository.findByEmail(userDetails.getUser().getEmail()).orElseThrow(
-            () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+            () -> new UserNotFoundException("등록된 사용자가 없습니다.")
         );
 
         user.unActivate();
