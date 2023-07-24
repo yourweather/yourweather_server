5fc116d2d226f2545a84dea1cd77e895d9c771d0
Jin Yu <94737714+HI-JIN2@users.noreply.github.com>
2023-07-25 02:19:24 +0900

    feat: 월별, 일별 데이터 찾기 get 메소드 작성

--- src/main/java/com/umc/yourweather/controller/WeatherController.java
+++ src/main/java/com/umc/yourweather/controller/WeatherController.java
@@ -7,10 +7,14 @@ import com.umc.yourweather.dto.ResponseDto;
 import com.umc.yourweather.dto.WeatherRequestDto;
 import com.umc.yourweather.service.WeatherService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +26,23 @@ public class WeatherController {
 
     @PostMapping("/create")
     public ResponseDto<Weather> create(@RequestBody @Valid WeatherRequestDto weatherRequestDto,
-        @AuthenticationPrincipal CustomUserDetails userDetails) {
+                                       @AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseDto.success(weatherService.create(weatherRequestDto, userDetails));
     }
-}
 
+//    @GetMapping("/find/byId/{id}")
+//    public ResponseDto<Weather> getDiaryEntryById(@PathVariable Long id) {
+//        return weatherService.getDiaryEntryById(id).orElse(null);
+//    }ㄹ
+
+    @GetMapping("/find/byDate")
+    public List<Weather> getWeathersByDate(@RequestParam("date") String date) {
+        LocalDate localDate = LocalDate.parse(date);
+        return weatherService.getWeathersByDate(localDate);
+    }
+
+    @GetMapping("/find/byMonth")
+    public List<Weather> getWeathersByMonth(@RequestParam("month") int month, @RequestParam("year") int year) {
+        return weatherService.getWeathersByMonth(month, year);
+    }
+}
