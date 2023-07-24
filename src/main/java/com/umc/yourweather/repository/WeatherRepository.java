64c751a78702be4f4f3383e12257326c01a0919e
Jin Yu <94737714+HI-JIN2@users.noreply.github.com>
2023-07-25 02:19:41 +0900

    feat: 레파지토리는 작업 중

--- src/main/java/com/umc/yourweather/repository/WeatherRepository.java
+++ src/main/java/com/umc/yourweather/repository/WeatherRepository.java
@@ -3,6 +3,14 @@ package com.umc.yourweather.repository;
 import com.umc.yourweather.domain.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+public interface WeatherRepository extends JpaRepository<Weather, Long> {
+//    void create(Weather entry);
+//    void delete(Long id);
+//    Optional<Weather> findById(Long id);
+    List<Weather> findByDate(LocalDate date);
+    List<Weather> findByMonth(int month, int year);
 }
