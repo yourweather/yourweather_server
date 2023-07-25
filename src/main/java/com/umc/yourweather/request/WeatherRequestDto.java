845e8c54b78b0aac1119bbb3638a625c971c2ab5
gundorit <packdev937@gmail.com>
2023-07-25 20:59:11 +0900

    fix : remove ambiguous variable and add year, month and day

--- src/main/java/com/umc/yourweather/request/WeatherRequestDto.java
+++ src/main/java/com/umc/yourweather/request/WeatherRequestDto.java
@@ -1,40 +1,19 @@
 package com.umc.yourweather.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.PrePersist;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class WeatherRequestDto {
 
-    @JsonFormat(pattern = "yyyyMMdd")
-    private String datetime;
-
-    private LocalDate date;
-
-    public int getYear() {
-        return date.getYear();
-    }
-
-    public int getMonth() {
-        return date.getMonthValue();
-    }
-
-    public int getDay() {
-        return date.getDayOfMonth();
-    }
-
-    @PrePersist
-    public void setDate(String datetime) {
-        date = LocalDate.parse(this.datetime, DateTimeFormatter.ofPattern("yyyyMMdd"));
-    }
+    int year;
+    int month;
+    int day;
 
     @Builder
-    public WeatherRequestDto(String datetime) {
-        this.datetime = datetime;
-        setDate(datetime);
+    public WeatherRequestDto(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
     }
 }
