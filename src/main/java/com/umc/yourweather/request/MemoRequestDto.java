1d6ce57b14cee234333019d2f5f8a5f3d3e061fc
gundorit <packdev937@gmail.com>
2023-07-25 20:56:50 +0900

    feat : add DATETIME variable; year, month, day, hour, minute, second

--- src/main/java/com/umc/yourweather/request/MemoRequestDto.java
+++ src/main/java/com/umc/yourweather/request/MemoRequestDto.java
@@ -1,12 +1,9 @@
 package com.umc.yourweather.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.umc.yourweather.domain.Status;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,26 +12,15 @@ public class MemoRequestDto {
 
     @Enumerated(EnumType.STRING)
     private Status status;
-
     @NotBlank
     private String content;
 
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+
+    private int second;
     private int temperature;
-
-    @PrePersist
-    public void setTime() {
-        date = LocalDate.parse(this.datetime, DateTimeFormatter.ofPattern("yyyyMMdd"));
-        this.time = LocalTime.now();
-    }
-
-    @Builder
-    public MemoRequestDto(String datetime, LocalDate date, LocalTime time, Status status,
-        String content, int condition) {
-        this.datetime = datetime;
-        this.date = date;
-        this.time = time;
-        this.status = status;
-        this.content = content;
-        this.condition = condition;
-    }
 }
