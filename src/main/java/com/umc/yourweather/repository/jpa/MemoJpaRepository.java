3e79078b7d4451407df66cfaafb4780cc5a95995
shon5544 <sbs8239@gmail.com>
2023-07-28 03:52:37 +0900

    fix: findByCreatedDateBetween 메서드 변경 - findByCreatedDateBetween -> findByUserAndCreatedDateBetween - 어떤 유저의 데이터인지도 알아야하기 때문에 이렇게 했음. - 해당 네이밍으로는 에러가 나서 별도의 Query를 작성했음.

--- src/main/java/com/umc/yourweather/repository/jpa/MemoJpaRepository.java
+++ src/main/java/com/umc/yourweather/repository/jpa/MemoJpaRepository.java
@@ -1,10 +1,25 @@
 package com.umc.yourweather.repository.jpa;
 
 import com.umc.yourweather.domain.entity.Memo;
+import com.umc.yourweather.domain.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemoJpaRepository extends JpaRepository<Memo, Long> {
-    List<Memo> findByCreatedDateBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+
+    @Query("SELECT m FROM Memo m "
+            + "JOIN FETCH m.weather w "
+            + "JOIN FETCH w.user u "
+            + "WHERE "
+            + "u = :user AND "
+            + "m.createdDate >= :startDateTime AND "
+            + "m.createdDate <= :endDateTime "
+            + "ORDER BY m.createdDate asc")
+    List<Memo> findByUserAndCreatedDateBetween(
+            @Param("user") User user,
+            @Param("startDateTime") LocalDateTime startDateTime,
+            @Param("endDateTime") LocalDateTime endDateTime);
 }
