6a97ea1aa4e6acf64c7de2275f8e9c672aa14c84
shon5544 <sbs8239@gmail.com>
2023-07-27 21:49:25 +0900

    fix: weather 없는 경우 새로 만들어주도록 변경

--- src/main/java/com/umc/yourweather/service/MemoService.java
+++ src/main/java/com/umc/yourweather/service/MemoService.java
@@ -3,9 +3,9 @@ package com.umc.yourweather.service;
 import com.umc.yourweather.auth.CustomUserDetails;
 import com.umc.yourweather.domain.Memo;
 import com.umc.yourweather.domain.Weather;
+import com.umc.yourweather.repository.MemoRepository;
 import com.umc.yourweather.request.MemoRequestDto;
 import com.umc.yourweather.response.MemoResponseDto;
-import com.umc.yourweather.repository.MemoRepository;
 import com.umc.yourweather.repository.WeatherRepository;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
@@ -20,25 +20,32 @@ public class MemoService {
 
     public MemoResponseDto write(MemoRequestDto memoRequestDto, CustomUserDetails userDetails) {
         LocalDate date = LocalDate.of(memoRequestDto.getYear(), memoRequestDto.getMonth(),
-            memoRequestDto.getDay());
-        Weather weather = weatherRepository.findByDate(LocalDate.of(
-                date.getYear(),
-                date.getMonthValue(), date.getDayOfMonth()))
-            .orElseThrow(() -> new RuntimeException("해당 날자에 날씨 객체가 존재하지 않습니다."));
+                memoRequestDto.getDay());
+
+        // weather 찾아보고 만약 없으면 새로 등록해줌.
+        Weather weather = weatherRepository.findByDate(date)
+                .orElseGet(() -> {
+                    Weather newWeather = Weather.builder()
+                            .user(userDetails.getUser())
+                            .date(date)
+                            .build();
+
+                    return weatherRepository.save(newWeather);
+                });
 
         // MemoRequestDto에 넘어온 정보를 토대로 Memo 객체 생성
         Memo memo = Memo.builder()
-            .weather(weather)
-            .status(memoRequestDto.getStatus())
-            .content(memoRequestDto.getContent())
-            .temperature(memoRequestDto.getTemperature())
-            .build();
+                .weather(weather)
+                .status(memoRequestDto.getStatus())
+                .content(memoRequestDto.getContent())
+                .temperature(memoRequestDto.getTemperature())
+                .build();
 
         memoRepository.save(memo);
         return MemoResponseDto.builder()
-            .status(memo.getStatus())
-            .content(memo.getContent())
-            .temperature(memo.getTemperature())
-            .build();
+                .status(memo.getStatus())
+                .content(memo.getContent())
+                .temperature(memo.getTemperature())
+                .build();
     }
 }
