9397fce03214e53f57d2b1f9d897fc1454255d6d
shon5544 <sbs8239@gmail.com>
2023-07-28 03:54:29 +0900

    fix: findByCreatedDateBetween 메서드 수정

--- src/main/java/com/umc/yourweather/repository/MemoRepository.java
+++ src/main/java/com/umc/yourweather/repository/MemoRepository.java
@@ -1,10 +1,13 @@
 package com.umc.yourweather.repository;
 
 import com.umc.yourweather.domain.entity.Memo;
+import com.umc.yourweather.domain.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository {
-    List<Memo> findByCreatedDateBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    List<Memo> findByUserAndCreatedDateBetween(User user, LocalDateTime startDateTime,
+            LocalDateTime endDateTime);
+
     Memo save(Memo memo);
 }
