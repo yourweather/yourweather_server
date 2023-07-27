82c203d22881e0911193d9511c05db5a57161454
shon5544 <sbs8239@gmail.com>
2023-07-28 00:46:07 +0900

    fix: 접근 제한자 설정 및 getSum 메서드 추가

--- src/main/java/com/umc/yourweather/domain/Statistic.java
+++ src/main/java/com/umc/yourweather/domain/Statistic.java
@@ -1,15 +1,17 @@
 package com.umc.yourweather.domain;
 
 import com.umc.yourweather.domain.enums.Status;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+@Getter
 @Slf4j
 public class Statistic {
 
-    int sunny;
-    int cloudy;
-    int rainy;
-    int lightning;
+    private int sunny;
+    private int cloudy;
+    private int rainy;
+    private int lightning;
 
     public void plusPoint(Status status) {
         switch(status){
@@ -19,5 +21,9 @@ public class Statistic {
             case LIGHTNING -> lightning++;
             default -> log.error("통계 값 갱신 실패: 올바른 status 값이 아닙니다.");
         }
+    }
+
+    public int getSum() {
+        return sunny + cloudy + rainy + lightning;
     }
 }
