5723eae5e0745f4c09749b06aaf916de25ad8ea8
shon5544 <sbs8239@gmail.com>
2023-07-27 21:48:53 +0900

    feat: 통계 데이터용 도메인 추가

---
+++ src/main/java/com/umc/yourweather/domain/Statistic.java
@@ -0,0 +1,22 @@
+package com.umc.yourweather.domain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Statistic {
+
+    int sunny;
+    int cloudy;
+    int rainy;
+    int lightning;
+
+    public void plusPoint(Status status) {
+        switch(status){
+            case SUNNY -> sunny++;
+            case CLOUDY -> cloudy++;
+            case RAINY -> rainy++;
+            case LIGHTNING -> lightning++;
+            default -> log.error("통계 값 갱신 실패: 올바른 status 값이 아닙니다.");
+        }
+    }
+}
