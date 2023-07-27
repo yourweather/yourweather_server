7c1ec98718d54ea4c74f3f3b1c7b500c84a6fb08
shon5544 <sbs8239@gmail.com>
2023-07-27 18:08:29 +0900

    feat: MemoRepository 구현체 추가

---
+++ src/main/java/com/umc/yourweather/repository/impl/MemoRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.umc.yourweather.repository.impl;
+
+import com.umc.yourweather.domain.Memo;
+import com.umc.yourweather.repository.MemoRepository;
+import com.umc.yourweather.repository.jpa.MemoJpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemoRepositoryImpl implements MemoRepository {
+
+    private final MemoJpaRepository memoJpaRepository;
+
+    @Override
+    public List<Memo> findByCreatedDateBetween(LocalDateTime startDateTime,
+            LocalDateTime endDateTime) {
+        return memoJpaRepository.findByCreatedDateBetween(startDateTime, endDateTime);
+    }
+}
