3a520f792a0f5041d4516ea5efb8317b3c79ad8f
shon5544 <sbs8239@gmail.com>
2023-07-28 03:55:07 +0900

    fix: findByCreatedDateBetween 수정

--- src/main/java/com/umc/yourweather/domain/StatisticCreator.java
+++ src/main/java/com/umc/yourweather/domain/StatisticCreator.java
@@ -1,6 +1,7 @@
 package com.umc.yourweather.domain;
 
 import com.umc.yourweather.domain.entity.Memo;
+import com.umc.yourweather.domain.entity.User;
 import com.umc.yourweather.repository.MemoRepository;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,13 +11,13 @@ import lombok.RequiredArgsConstructor;
 public class StatisticCreator {
     private final MemoRepository memoRepository;
 
-    public Statistic createWeeklyStatistic(LocalDateTime dateTime) {
+    public Statistic createWeeklyStatistic(User user, LocalDateTime dateTime) {
         int dayOfWeek = dateTime.getDayOfWeek().getValue();
 
         LocalDateTime startDateTime = dateTime.minusDays(dayOfWeek - 1);
         LocalDateTime endDateTime = dateTime.plusDays(7 - dayOfWeek);
 
-        List<Memo> memoList = memoRepository.findByCreatedDateBetween(startDateTime,
+        List<Memo> memoList = memoRepository.findByUserAndCreatedDateBetween(user, startDateTime,
                 endDateTime);
 
         Statistic statistic = new Statistic();
