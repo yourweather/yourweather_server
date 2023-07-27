123b8cd0414dcc5b722143f027156059f1c8c5dc
shon5544 <sbs8239@gmail.com>
2023-07-28 00:45:02 +0900

    feat: 통계 관리 도메인 생성

---
+++ src/main/java/com/umc/yourweather/domain/StatisticCreator.java
@@ -0,0 +1,28 @@
+package com.umc.yourweather.domain;
+
+import com.umc.yourweather.domain.entity.Memo;
+import com.umc.yourweather.repository.MemoRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class StatisticCreator {
+    private final MemoRepository memoRepository;
+
+    public Statistic createWeeklyStatistic(LocalDateTime dateTime) {
+        int dayOfWeek = dateTime.getDayOfWeek().getValue();
+
+        LocalDateTime startDateTime = dateTime.minusDays(dayOfWeek - 1);
+        LocalDateTime endDateTime = dateTime.plusDays(7 - dayOfWeek);
+
+        List<Memo> memoList = memoRepository.findByCreatedDateBetween(startDateTime,
+                endDateTime);
+
+        Statistic statistic = new Statistic();
+
+        memoList.forEach(memo -> statistic.plusPoint(memo.getStatus()));
+
+        return statistic;
+    }
+}
