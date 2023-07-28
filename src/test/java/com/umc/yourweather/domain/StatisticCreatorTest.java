4806a9aac92d97e31d31971e2eb694b3a187d9c6
shon5544 <sbs8239@gmail.com>
2023-07-28 18:39:01 +0900

    feat: 월간 통계 수치화 테스트 추가

--- src/test/java/com/umc/yourweather/domain/StatisticCreatorTest.java
+++ src/test/java/com/umc/yourweather/domain/StatisticCreatorTest.java
@@ -7,6 +7,7 @@ import com.umc.yourweather.repository.MemoRepository;
 import com.umc.yourweather.repository.test.MemoTestRepository;
 import com.umc.yourweather.response.StatisticResponseDto;
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,25 +17,14 @@ class StatisticCreatorTest {
 
     MemoRepository memoRepository = new MemoTestRepository(7);
 
-    @Test
-    @DisplayName("통계 생성 테스트")
-    public void test1() {
-        // given
-        StatisticCreator statisticCreator = new StatisticCreator(memoRepository);
-        User user = User.builder()
-                .email("sbs8239@gmail.com")
-                .build();
-
-        // when
-        Statistic weeklyStatistic = statisticCreator.createWeeklyStatistic(user, LocalDateTime.now());
-
-        // then
-        System.out.println(weeklyStatistic.toString());
+    @BeforeEach
+    public void setMemoRepository() {
+        memoRepository = new MemoTestRepository(7);
     }
 
     @Test
-    @DisplayName("통계 수치화 테스트")
-    public void test2() {
+    @DisplayName("주간 통계 수치화 테스트")
+    public void test1() {
         // given
         StatisticCreator statisticCreator = new StatisticCreator(memoRepository);
         User user = User.builder()
@@ -54,6 +44,32 @@ class StatisticCreatorTest {
         System.out.println(statisticResponseDto.toString());
 
         assertEquals(weeklyStatistic.getSum(), 7);
+        assertEquals(sum, 100);
+    }
+
+    @Test
+    @DisplayName("월간 통계 수치화 테스트")
+    public void test2() {
+        // given
+        memoRepository = new MemoTestRepository(31);
+        StatisticCreator statisticCreator = new StatisticCreator(memoRepository);
+        User user = User.builder()
+                .email("sbs8239@gmail.com")
+                .build();
+
+        // when
+        Statistic weeklyStatistic = statisticCreator.createWeeklyStatistic(user, LocalDateTime.now());
+        StatisticResponseDto statisticResponseDto = new StatisticResponseDto(weeklyStatistic);
+        int sum = Math.round(statisticResponseDto.getSunny() +
+                statisticResponseDto.getCloudy() +
+                statisticResponseDto.getRainy() +
+                statisticResponseDto.getLightning());
+
+        // then
+        System.out.println(weeklyStatistic);
+        System.out.println(statisticResponseDto.toString());
+
+        assertEquals(weeklyStatistic.getSum(), 31);
         assertEquals(sum, 100);
     }
 }
