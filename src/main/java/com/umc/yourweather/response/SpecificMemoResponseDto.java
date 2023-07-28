3c1b0525b4ba4704a9cda71bbbc24b8df140376d
shon5544 <sbs8239@gmail.com>
2023-07-28 22:26:12 +0900

    feat: Getter, ToString 추가

--- src/main/java/com/umc/yourweather/response/SpecificMemoResponseDto.java
+++ src/main/java/com/umc/yourweather/response/SpecificMemoResponseDto.java
@@ -4,11 +4,15 @@ import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@ToString
+@Getter
 public class SpecificMemoResponseDto {
     private List<MemoReportResponseDto> memoList;
     private float proportion;
