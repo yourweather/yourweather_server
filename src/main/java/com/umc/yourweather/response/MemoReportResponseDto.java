344be2998f314cecc19e0cd5f0e0680f95b8227a
shon5544 <sbs8239@gmail.com>
2023-07-28 22:26:12 +0900

    feat: Getter, ToString 추가

--- src/main/java/com/umc/yourweather/response/MemoReportResponseDto.java
+++ src/main/java/com/umc/yourweather/response/MemoReportResponseDto.java
@@ -3,15 +3,19 @@ package com.umc.yourweather.response;
 import com.umc.yourweather.domain.entity.Memo;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Getter
 public class MemoReportResponseDto {
     private Long memoId;
     private LocalDateTime dateTime;
 
     public MemoReportResponseDto(Memo entity) {
         memoId = entity.getId();
-        dateTime = entity.getCreatedDate();
+        dateTime = entity.getCreatedDateTime();
     }
 }
