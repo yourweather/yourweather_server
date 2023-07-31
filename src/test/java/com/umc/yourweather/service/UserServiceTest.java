8640352be82a27c22418d2fd026db0b9c93540e0
gundorit <packdev937@gmail.com>
2023-07-31 16:01:37 +0900

    test : 마이페이지, 비밀번호 변경, 회원 탈퇴에 대한 Service Layer 검증 Test 추가

--- src/test/java/com/umc/yourweather/service/UserServiceTest.java
+++ src/test/java/com/umc/yourweather/service/UserServiceTest.java
@@ -3,9 +3,13 @@ package com.umc.yourweather.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.umc.yourweather.auth.CustomUserDetails;
+import com.umc.yourweather.domain.Role;
 import com.umc.yourweather.domain.User;
 import com.umc.yourweather.repository.UserRepository;
+import com.umc.yourweather.request.ChangePasswordRequestDto;
 import com.umc.yourweather.request.SignupRequestDto;
+import com.umc.yourweather.response.UserResponseDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -46,11 +50,80 @@ class UserServiceTest {
             .build();
 
         // when
-        userService.signup(request);
+        User user = userService.signup(request);
 
         // then
-        User user = userRepository.findByEmail("test@test.com")
-            .orElseThrow(() -> new RuntimeException("Error"));
         assertEquals(user.getEmail(), request.getEmail());
+    }
+
+    @Test
+    @DisplayName("mypage 조회")
+    void mypage() {
+        // given
+        User user = User.builder()
+            .email("test@test.com")
+            .password("password")
+            .nickname("nickname")
+            .platform("platform")
+            .role(Role.ROLE_USER)
+            .isActivate(true)
+            .build();
+
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        // when
+        UserResponseDto response = userService.mypage(userDetails);
+
+        // then
+        assertEquals(userDetails.getUser().getEmail(), response.getEmail());
+        assertEquals(userDetails.getUser().getNickname(), response.getNickname());
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경")
+    void changePassword(){
+        // given
+        ChangePasswordRequestDto request = new ChangePasswordRequestDto("password2");
+
+        User user = User.builder()
+            .email("test@test.com")
+            .password("password")
+            .nickname("nickname")
+            .platform("platform")
+            .role(Role.ROLE_USER)
+            .isActivate(true)
+            .build();
+
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        // when
+        userService.changePassword(request, userDetails);
+
+        // then
+        User findUser = userRepository.findByEmail("test@test.com").get();
+        assertEquals(findUser.getPassword(), "password2");
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴")
+    void withdraw(){
+        //given
+        User user = User.builder()
+            .email("test@test.com")
+            .password("password")
+            .nickname("nickname")
+            .platform("platform")
+            .role(Role.ROLE_USER)
+            .isActivate(true)
+            .build();
+
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        // when
+        userService.withdraw(userDetails);
+
+        // then
+        User findUser = userRepository.findByEmail("test@test.com").get();
+        assertEquals(findUser.isActivate(), false);
     }
 }
