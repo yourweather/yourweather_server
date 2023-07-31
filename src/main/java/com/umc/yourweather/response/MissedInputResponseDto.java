f0eb3b0b67e72f62174b286ae9827d6a8ce6eeb5
gundorit <packdev937@gmail.com>
2023-07-31 17:26:40 +0900

    feat : @Getter 추가

--- src/main/java/com/umc/yourweather/response/MissedInputResponseDto.java
+++ src/main/java/com/umc/yourweather/response/MissedInputResponseDto.java
@@ -3,7 +3,9 @@ package com.umc.yourweather.response;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
 
+@Getter
 public class MissedInputResponseDto {
 
     List<LocalDate> localDates = new ArrayList<>();
