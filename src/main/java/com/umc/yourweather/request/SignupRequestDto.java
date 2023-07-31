184e7ca03c35b07a51922d64556d28235224931e
shon5544 <sbs8239@gmail.com>
2023-08-01 00:00:46 +0900

    fix: import 누락 수정

--- src/main/java/com/umc/yourweather/request/SignupRequestDto.java
+++ src/main/java/com/umc/yourweather/request/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.umc.yourweather.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
