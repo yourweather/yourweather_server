b84ceee86222386ca295fe1447300b78b306c325
shon5544 <sbs8239@gmail.com>
2023-08-01 11:56:53 +0900

    fix: Swagger 오류 수정

--- src/main/java/com/umc/yourweather/config/SpringDocsConfig.java
+++ src/main/java/com/umc/yourweather/config/SpringDocsConfig.java
@@ -1,13 +1,10 @@
 package com.umc.yourweather.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
