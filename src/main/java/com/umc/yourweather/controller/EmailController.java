6a26f4fd81eee400923db4feaa9c99c5941a7a84
shon5544 <sbs8239@gmail.com>
2023-08-02 19:51:16 +0900

    feat: EmailController Operation 추가

--- src/main/java/com/umc/yourweather/controller/EmailController.java
+++ src/main/java/com/umc/yourweather/controller/EmailController.java
@@ -3,6 +3,9 @@ package com.umc.yourweather.controller;
 import com.umc.yourweather.api.RequestURI;
 import com.umc.yourweather.response.ResponseDto;
 import com.umc.yourweather.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +23,8 @@ public class EmailController {
     private final EmailService emailService;
 
     @GetMapping("/send")
+    @Operation(summary = "이메일 전송", description = "인증을 위한 이메일을 전송합니다. 인증코드를 유저로부터 입력받아 /certify 요청을 보내면 유효한 코드인지 알 수 있습니다.")
+    @Parameter(name = "email", description = "메일을 보낼 사용자 이메일입니다.", in = ParameterIn.QUERY)
     public ResponseDto<Void> send(@RequestParam String email) {
         try {
             CompletableFuture<String> completableFutureCode = emailService.sendMessage(email);
@@ -31,6 +36,9 @@ public class EmailController {
     }
 
     @GetMapping("/certify")
+    @Operation(summary = "이메일 인증", description = "이메일로부터 전송받은 인증 코드를 인증합니다. result 값이 true면 인증이 완료된 것입니다.")
+    @Parameter(name = "email", description = "인증코드를 받은 email 주소")
+    @Parameter(name = "code", description = "인증코드")
     public ResponseDto<Boolean> certify(@RequestParam String email, @RequestParam String code) {
         boolean isCorrect = emailService.certifyingData(email, code);
         return isCorrect ?
