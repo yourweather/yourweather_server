6379a7345dfa00852f1d1dee5de6bdff247c6418
shon5544 <sbs8239@gmail.com>
2023-08-03 13:33:58 +0900

    fix: NoArgsConstructor 추가

--- src/main/java/com/umc/yourweather/request/ChangePasswordRequestDto.java
+++ src/main/java/com/umc/yourweather/request/ChangePasswordRequestDto.java
@@ -2,15 +2,16 @@ package com.umc.yourweather.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ChangePasswordRequestDto {
 
     @NotBlank
     String password;
 
-//    public ChangePasswordRequestDto(String password) {
-//        this.password = password;
-//    }
+    public ChangePasswordRequestDto(String password) {
+        this.password = password;
+    }
 }
