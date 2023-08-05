442c976dbb0d397d2a40e6eade138caab1343722
gundorit <packdev937@gmail.com>
2023-08-06 02:27:55 +0900

    feat : add @NoArgsConstructor and Constructor

--- src/main/java/com/umc/yourweather/request/MissedInputRequestDto.java
+++ src/main/java/com/umc/yourweather/request/MissedInputRequestDto.java
@@ -2,10 +2,15 @@ package com.umc.yourweather.request;
 
 import java.time.LocalDate;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class MissedInputRequestDto {
 
     LocalDate date;
 
+    public MissedInputRequestDto(LocalDate date) {
+        this.date = date;
+    }
 }
