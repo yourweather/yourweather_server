057a6fc3f20c237bbf722f59b8fcddf54cdb39e2
shon5544 <sbs8239@gmail.com>
2023-08-07 20:43:56 +0900

    feat: RedisRepository에 사용할 엔티티 추가

---
+++ src/main/java/com/umc/yourweather/domain/entity/EmailCertify.java
@@ -0,0 +1,44 @@
+package com.umc.yourweather.domain.entity;
+
+import lombok.Builder;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@RedisHash(value = "email_certify")
+@ToString
+public class EmailCertify {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private final String email;
+
+    @TimeToLive
+    private final long liveSpan;
+
+    private String code;
+    private int count;
+
+    @Builder
+    public EmailCertify(String email, String code, long liveSpan) {
+        this.email = email;
+        this.code = code;
+        this.liveSpan = liveSpan;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void certifyCodeRenewal(String code) {
+        if(code == null)
+            throw new IllegalArgumentException("이메일 인증 요청 갱신 실패: 인자로 들어온 이메일 인증 코드가 null입니다.");
+
+        this.code = code;
+        this.count++;
+    }
+}
