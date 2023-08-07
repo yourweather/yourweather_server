9c5f66eaeebb80cefecad638a97bcab41628fbd6
shon5544 <sbs8239@gmail.com>
2023-08-07 20:44:48 +0900

    style: 배열 요소 들여쓰기 적용

--- src/main/java/com/umc/yourweather/exception/GlobalExceptionHandler.java
+++ src/main/java/com/umc/yourweather/exception/GlobalExceptionHandler.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({IllegalArgumentException.class, UserNotFoundException.class,
-        RuntimeException.class, WeatherNotFoundException.class})
+    @ExceptionHandler({
+            IllegalArgumentException.class,
+            UserNotFoundException.class,
+            RuntimeException.class,
+            WeatherNotFoundException.class,
+    })
     public ResponseDto<?> handler(Exception e) {
         return ResponseDto.fail(HttpStatus.BAD_REQUEST, e.getMessage());
     }
