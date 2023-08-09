f9289b704dfdbd879c35e9679c977c97066b2489
Yujin <jini1514@soongsil.ac.kr>
2023-08-09 17:31:35 +0900

    refactoring: WeatherRepository / Jpa / Impl 분리함

--- src/main/java/com/umc/yourweather/repository/WeatherRepository.java
+++ src/main/java/com/umc/yourweather/repository/WeatherRepository.java
@@ -3,15 +3,18 @@ package com.umc.yourweather.repository;
 import com.umc.yourweather.domain.entity.Memo;
 import com.umc.yourweather.domain.entity.User;
 import com.umc.yourweather.domain.entity.Weather;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
+import com.umc.yourweather.response.WeatherItemResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
+public interface WeatherRepository {
 
     List<Weather> findWeatherByDateBetween(LocalDate startDate, LocalDate endDate);
 
@@ -21,16 +24,7 @@ public interface WeatherRepository extends JpaRepository<Weather, Long> {
 
     List<WeatherItemResponseDto> findByMonthAndUser(User user, LocalDate startDate, LocalDate endDate);
 
+    Weather save(Weather weather);
 
-    @Query("SELECT w FROM Weather w "
-            + "JOIN FETCH w.user u "
-            + "WHERE "
-            + "u = :user AND "
-            + "w.date >= :startDate AND "
-            + "w.date <= :endDate "
-            + "ORDER BY w.date asc")
-    List<Weather> findByMonthAndUser(
-            @Param("user") User user,
-            @Param("startDate") LocalDate startDate,
-            @Param("endDate") LocalDate endDate);
+    void delete(Weather weather);
 }
