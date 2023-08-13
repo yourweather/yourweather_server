7b181f6d1e767759099b1d3f14d3a7b112119529
gundorit <packdev937@gmail.com>
2023-08-13 17:11:37 +0900

    feat : deploy.sh 내용 업데이트

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+REPOSITORY=/home/ec2-user
+
+cd &REPOSITORY
+
+APP_NAME=your_weather
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> No application to stop."
+else
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploying - $JAR_PATH"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
