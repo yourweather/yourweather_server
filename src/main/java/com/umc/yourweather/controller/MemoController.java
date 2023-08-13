e0166bf0e217af88a6bae981a0dd6c45d8569970
Yujin <jini1514@soongsil.ac.kr>
2023-08-13 13:05:19 +0900

    docs: api 다 빼는걸로 통일

--- src/main/java/com/umc/yourweather/controller/MemoController.java
+++ src/main/java/com/umc/yourweather/controller/MemoController.java
@@ -23,21 +23,21 @@ public class MemoController {
     private final MemoService memoService;
 
     @PostMapping("/write")
-    @Operation(summary = "메모 작성 api", description = "메모 작성을 위한 API입니다. 전달받은 요청 데이터들을 참조하여 메모를 저장합니다.")
+    @Operation(summary = "메모 작성", description = "메모 작성을 위한 API입니다. 전달받은 요청 데이터들을 참조하여 메모를 저장합니다.")
     public ResponseDto<MemoResponseDto> write(@RequestBody @Valid MemoRequestDto memoRequestDto
             , @AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseDto.success("메모 저장 완료", memoService.write(memoRequestDto, userDetails));
     }
 
     @GetMapping("/daily/{weatherId}")
-    @Operation(summary = "하루 치 메모 반환 api", description = "하루치 메모 리스트를 반환하기 위한 API입니다. weather-controller에 있는 monthly API의 반환 값에 있는 weather_id를 이용해서 조회합니다. ")
+    @Operation(summary = "하루 치 메모 반환", description = "하루치 메모 리스트를 반환하기 위한 API입니다. weather-controller에 있는 monthly API의 반환 값에 있는 weatherIzzd를 이용해서 조회합니다. ")
     public ResponseDto<MemoDailyResponseDto> daily(@PathVariable Long weatherId,
                                                    @AuthenticationPrincipal CustomUserDetails userDetails) {
 
         return ResponseDto.success(weatherId + "번의 메모 리스트 조회 성공", memoService.getDailyList(weatherId, userDetails));
     }
 
-    @Operation(summary = "하루 치 메모 반환 api", description = "특정 메모를 반환하기 위한 API입니다. 특정 날씨 List의 반환 값에 있는 memoId를 이용해서 조회합니다. ")
+    @Operation(summary = "하루 치 메모 반환", description = "특정 메모를 반환하기 위한 API입니다. 특정 날씨 List의 반환 값에 있는 memoId를 이용해서 조회합니다. ")
     @GetMapping("/{memoId}")
     public ResponseDto<MemoResponseDto> getMemo(@PathVariable Long memoId) {
 
@@ -45,13 +45,13 @@ public class MemoController {
     }
 
 
-    @Operation(summary = "메모 수정 api", description = "메모 수정을 위한 API입니다.")
+    @Operation(summary = "메모 수정", description = "메모 수정을 위한 API입니다.")
     @PutMapping("/update/{memoId}")
     public ResponseDto<MemoUpdateResponseDto> update(@PathVariable Long memoId, @RequestBody MemoUpdateRequestDto requestDto) {
         return ResponseDto.success("메모 수정 완료", memoService.update(memoId, requestDto));
     }
 
-    @Operation(summary = "메모 삭제 api", description = "메모 삭제을 위한 API입니다.")
+    @Operation(summary = "메모 삭제", description = "메모 삭제을 위한 API입니다.")
     @DeleteMapping("/delete/{memoId}")
     public ResponseDto<Void> delete(@PathVariable Long memoId) {
         memoService.delete(memoId);
--- src/main/java/com/umc/yourweather/controller/WeatherController.java
+++ src/main/java/com/umc/yourweather/controller/WeatherController.java
@@ -20,14 +20,14 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping("/home")
-    @Operation(summary = "홈 조회 api", description = "홈 화면 데이터를 조회하는 API 입니다.")
+    @Operation(summary = "홈 조회", description = "홈 화면 데이터를 조회하는 API 입니다.")
     public ResponseDto<HomeResponseDto> home(
         @AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseDto.success("홈 데이터 조회 성공", weatherService.home(userDetails));
     }
 
     @GetMapping("/no-inputs")
-    @Operation(summary = "미 입력 조회 api", description = "최근 일주일 간 미 입력 날짜를 조회하는 API 입니다.")
+    @Operation(summary = "미 입력 조회", description = "최근 일주일 간 미 입력 날짜를 조회하는 API 입니다.")
     public ResponseDto<MissedInputResponseDto> getMissedInputs(
         @AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseDto.success("미 입력 날짜 조회 성공",
@@ -43,7 +43,7 @@ public class WeatherController {
     }
 
     @DeleteMapping("/{year}-{month}-{day}")
-    @Operation(summary = "Weather 삭제 api", description = "Weather 삭제 API 입니다. 전달 받은 날짜에 해당하는 Weather 객체를 삭제합니다.")
+    @Operation(summary = "Weather 삭제", description = "Weather 삭제 API 입니다. 전달 받은 날짜에 해당하는 Weather 객체를 삭제합니다.")
     public ResponseDto<WeatherResponseDto> delete(@PathVariable int year,
         @PathVariable int month,
         @PathVariable int day,
