39bda3b68cc200473c417744c3d54583d9eaec10
shon5544 <sbs8239@gmail.com>
2023-08-14 21:40:10 +0900

    style: import 와일드카드 제거

--- src/main/java/com/umc/yourweather/domain/entity/User.java
+++ src/main/java/com/umc/yourweather/domain/entity/User.java
@@ -2,16 +2,25 @@ package com.umc.yourweather.domain.entity;
 
 import com.umc.yourweather.domain.enums.Platform;
 import com.umc.yourweather.domain.enums.Role;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
