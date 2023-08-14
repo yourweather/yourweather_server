f4efba9b48ac008712d26dcf88299ffbba8b55cb
shon5544 <sbs8239@gmail.com>
2023-08-14 20:40:48 +0900

    feat: company, summary 추가

--- src/main/java/com/umc/yourweather/domain/entity/Advertisement.java
+++ src/main/java/com/umc/yourweather/domain/entity/Advertisement.java
@@ -5,18 +5,31 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Advertisement {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String company;
+    private String summary;
     private String message;
     private String url;
+
+
+    @Builder
+    public Advertisement(String company, String summary, String message, String url) {
+        this.company = company;
+        this.summary = summary;
+        this.message = message;
+        this.url = url;
+    }
 
     public String getMessage() {
         return message;
--- src/main/java/com/umc/yourweather/request/AddAdvertisementRequestDto.java
+++ src/main/java/com/umc/yourweather/request/AddAdvertisementRequestDto.java
@@ -1,11 +1,33 @@
 package com.umc.yourweather.request;
 
+import com.umc.yourweather.domain.entity.Advertisement;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class AddAdvertisementRequestDto {
+
+    @NotBlank
+    private String company;
+
+    @NotBlank
+    private String summary;
+
+    @NotBlank
     private String message;
+
+    @NotBlank
     private String url;
+
+
+    public Advertisement toEntity() {
+        return Advertisement.builder()
+                .company(company)
+                .summary(summary)
+                .message(message)
+                .url(url)
+                .build();
+    }
 }
