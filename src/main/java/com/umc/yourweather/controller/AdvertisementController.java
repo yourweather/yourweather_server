8a93afb3b13f9947f4639059b6b3a9bd4e83a1ef
shon5544 <sbs8239@gmail.com>
2023-08-14 21:12:53 +0900

    feat: 광고 페이지 추가

--- src/main/java/com/umc/yourweather/controller/AdvertisementController.java
+++ src/main/java/com/umc/yourweather/controller/AdvertisementController.java
@@ -7,21 +7,24 @@ import com.umc.yourweather.response.ResponseDto;
 import com.umc.yourweather.service.AdvertisementService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping(RequestURI.ADVERTISEMENT_URI)
 @RequiredArgsConstructor
 public class AdvertisementController {
     private final AdvertisementService advertisementService;
 
     @PostMapping("/add-advertisement")
+    @ResponseBody
     @Operation(summary = "광고 추가 api", description = "광고를 추가하는 api입니다. 어드민 권한이 있어야지만 실행 가능합니다.")
     public ResponseDto<AddAdvertisementResponseDto> addAdvertisement(
             @RequestBody AddAdvertisementRequestDto addAdvertisementRequestDto) {
@@ -32,6 +35,7 @@ public class AdvertisementController {
     }
 
     @PutMapping("/edit-advertisement")
+    @ResponseBody
     @Operation(summary = "광고 수정 api", description = "광고를 수정하는 api입니다. 어드민 권한이 있어야지만 실행 가능합니다.")
     public ResponseDto<AddAdvertisementResponseDto> editAdvertisement(
             @RequestBody AddAdvertisementRequestDto addAdvertisementRequestDto) {
@@ -42,11 +46,18 @@ public class AdvertisementController {
     }
 
     @DeleteMapping("/delete-advertisement")
+    @ResponseBody
     @Operation(summary = "광고 수정 api", description = "광고를 수정하는 api입니다. 어드민 권한이 있어야지만 실행 가능합니다.")
     public ResponseDto<Void> deleteAdvertisement(
             @RequestParam String adId) {
         advertisementService.deleteAdvertisement(adId);
 
         return ResponseDto.success("광고 삭제 성공");
+    }
+
+    @GetMapping("/get-advertisement")
+    @Operation(summary = "광고 제공 api", description = "광고를 가져오는 api입니다")
+    public String getAdvertisement() {
+        return "광고 페이지";
     }
 }
