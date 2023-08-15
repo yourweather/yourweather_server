9ae05e8d9ec46045ce01bacf86097bd48f732b46
shon5544 <sbs8239@gmail.com>
2023-08-16 05:47:15 +0900

    feat: Ec2를 위한 Timezone 설정

--- src/main/java/com/umc/yourweather/YourweatherApplication.java
+++ src/main/java/com/umc/yourweather/YourweatherApplication.java
@@ -2,6 +2,8 @@ package com.umc.yourweather;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +14,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @OpenAPIDefinition(servers = {@Server(url = "https://api.yourweather.shop/", description = "Default Server URL")})
 @SpringBootApplication
 public class YourweatherApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(YourweatherApplication.class, args);
