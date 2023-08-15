7ea0fc90981797a7738a22d37284083527bc2f00
shon5544 <sbs8239@gmail.com>
2023-08-15 23:58:23 +0900

    fix: https 변경 후 스웨거에서 동작하도록 수정

--- src/main/java/com/umc/yourweather/YourweatherApplication.java
+++ src/main/java/com/umc/yourweather/YourweatherApplication.java
@@ -1,5 +1,7 @@
 package com.umc.yourweather;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -7,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
+@OpenAPIDefinition(servers = {@Server(url = "https://api.yourweather.shop/", description = "Default Server URL")})
 @SpringBootApplication
 public class YourweatherApplication {
 
